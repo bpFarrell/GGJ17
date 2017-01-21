3973fb3fcd187c5f99145339c3a71b21ed1b329d
Rojibora <Rojibora@gmail.com>
2017-01-21 15:07:17 -0800

    Moved state machine functions to GameStateManager.cs Tested and patched state machine functionality.

--- Assets/GameManagerScript.cs
+++ Assets/GameManagerScript.cs
@@ -4,44 +4,8 @@ using UnityEngine;
 
 public class GameManagerScript : MonoBehaviour 
 {
-
 	static public GameManagerScript _MAIN;
-
-	public enum STATE 
-	{
-		INIT  = 0,
-		PLAY  = 1,
-		END   = 2,
-		RESET = 3
-	}
-
-	public STATE eGameState = STATE.INIT;
-
-	public delegate void StateDelegate();
-
-	public class genericState
-	{
-		public bool Initializated = false;
-		public STATE type;
-		public genericState(STATE enu)
-		{
-			type = enu;
-		}
-
-		public StateDelegate StartOfState = delegate {};
-		public StateDelegate UpdateState = delegate {};
-		public StateDelegate ExitState = delegate {};
-	}
-
-	public genericState Init;
-	public genericState Play;
-	public genericState End;
-	public genericState Reset;
-
-	private genericState[] stateContainer;
-
-	private StateDelegate updateDelegate = delegate {};
-
+	
 	// Use this for initialization
 	void Start ()
 	{
@@ -53,37 +17,34 @@ public class GameManagerScript : MonoBehaviour
 			Destroy (this);
 		}
 
-		stateContainer = new genericState[] {Init, Play, End, Reset};
-	}
+		GameStateManager._MAIN.NullState.StartOfState += CreatePlayers;
+		GameStateManager._MAIN.NullState.StartOfState += CreateScorekeeper;
+		GameStateManager._MAIN.NullState.StartOfState += CreateFishManager;
 
-	void ChangeState(STATE state)
-	{
-		stateContainer[((int) eGameState)].ExitState();
-		eGameState = state;
-		stateContainer[((int) eGameState)].StartOfState();
-		updateDelegate = stateContainer[((int) eGameState)].UpdateState;
+		GameStateManager._MAIN.ChangeState(GameStateManager.STATE.NULL);
 	}
 
 	// Update is called once per frame
 	void Update () 
 	{
-		updateDelegate();
-		/*switch (eGameState) 
-		{
-		default:
-			break;
-		case STATE.INIT:
-			InitDelegate();
-			break;
-		case STATE.PLAY:
-			PlayDelegate();
-			break;
-		case STATE.END:
-			EndDelegate();
-			break;
-		case STATE.RESET:
-			ResetDelegate();
-			break;
-		}*/
+
+	}
+	
+	public void CreatePlayers()
+	{
+		Debug.Log ("Players Initialized");
+		return;
+	}
+
+	public void CreateScorekeeper()
+	{
+		Debug.Log ("Scorekeeper Initialized");
+		return;
+	}
+
+	public void CreateFishManager()
+	{
+		Debug.Log ("FishManager Initialized");
+		return;
 	}
 }
---
+++ Assets/GameStateManager.cs
@@ -0,0 +1,99 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameStateManager : MonoBehaviour {
+
+	static public GameStateManager _MAIN;
+	
+	public enum STATE 
+	{
+		NULL  = 0,
+		INIT  = 1,
+		PLAY  = 2,
+		END   = 3,
+		RESET = 4
+	}
+	
+	public STATE eGameState = STATE.NULL;
+	
+	public delegate void StateDelegate();
+	
+	public class genericState
+	{
+		public genericState()
+		{
+			StartOfState = delegate {};
+			UpdateState = delegate {};
+			ExitState = delegate {};
+		}
+		public StateDelegate StartOfState;
+		public StateDelegate UpdateState;
+		public StateDelegate ExitState;
+	}
+	
+	public genericState NullState = new genericState();
+	public genericState Init = new genericState();
+	public genericState Play = new genericState();
+	public genericState End = new genericState();
+	public genericState Reset = new genericState();
+	
+	private genericState[] stateContainer;
+	
+	private StateDelegate updateDelegate = delegate {};
+	
+	// Use this for initialization
+	void Start ()
+	{
+		if (GameStateManager._MAIN == null)
+			GameStateManager._MAIN = this;
+		else
+		{
+			Debug.LogError("Multiple GameStateManager exist in scene: Deleting extra instance.");
+			Destroy (this);
+		}
+		
+		stateContainer = new genericState[] {NullState, Init, Play, End, Reset};
+	}
+	
+	public void ChangeState(STATE state)
+	{
+		if (state == null) 
+		{
+			Debug.LogError("ChangeState in GameStateController was given null parameter.");
+			return;
+		}
+		if (state == STATE.NULL)
+		{
+			Debug.Log ("GameState is entering NULL state.");
+		}
+		
+		stateContainer[((int) eGameState)].ExitState();
+		eGameState = state;
+		stateContainer[((int) eGameState)].StartOfState();
+		updateDelegate = stateContainer[((int) eGameState)].UpdateState;
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		updateDelegate();
+		/*switch (eGameState)
+		{
+		default:
+			break;
+		case STATE.INIT:
+			InitDelegate();
+			break;
+		case STATE.PLAY:
+			PlayDelegate();
+			break;
+		case STATE.END:
+			EndDelegate();
+			break;
+		case STATE.RESET:
+			ResetDelegate();
+			break;
+		}*/
+	}
+}
