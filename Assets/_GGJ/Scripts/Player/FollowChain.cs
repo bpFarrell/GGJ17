be57b761da4cfd631fdd11bfe75e75fc2a8d80c5
Brandon Farrell <brandonf@bentimagelab.com>
2017-01-21 04:35:17 -0800

    Rigg'n times!

--- Assets/_GGJ/Scripts/Player/FollowChain.cs
+++ Assets/_GGJ/Scripts/Player/FollowChain.cs
@@ -13,8 +13,17 @@ public class FollowChain : MonoBehaviour {
     }
     void Update() {
         if (Prev == null) return;
+        Vector3 dir = transform.position - Prev.transform.position;
+        float dist = dir.magnitude;
+        dir.Normalize();
+        if (dist < distance) {
+            transform.position = Prev.transform.position + dir * distance;
+        }
         transform.position = Vector3.Lerp(transform.position, Prev.transform.position + Prev.transform.forward * 1, Time.deltaTime * 10);
-        transform.rotation = Quaternion.Lerp(transform.rotation, Prev.transform.rotation, Time.deltaTime * 10);
+        //transform.rotation = Quaternion.Lerp(transform.rotation, Prev.transform.rotation, Time.deltaTime * 10);
+        transform.rotation = Quaternion.LookRotation(dir,transform.up);
+        float rDotu = Vector3.Dot(transform.right, Prev.transform.up);
+        transform.Rotate(0, 0, -rDotu*Time.deltaTime*200);
     }
     public void Spawn(FollowChain from) {
         if (Prev != null) {
---
+++ Assets/_GGJ/Scripts/Player/FollowChainFinal.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowChainFinal : MonoBehaviour {
+    public int length=-1;
+    public float distance {
+        get { return offset.magnitude; }
+    }
+
+    public FollowChainFinal Next;
+    public FollowChainFinal Prev;
+    public GameObject joint;
+    public Vector3 offset;
+    void Awake() {
+        if (length == -1) return;
+        Spawn(null);
+    }
+    void Update() {
+        if (Prev == null) return;
+        Vector3 dir = transform.position - Prev.transform.position;
+        float dist = dir.magnitude;
+        dir.Normalize();
+        if (dist < distance) {
+            transform.position = Prev.transform.position + dir * distance;
+        }
+        transform.position = Vector3.Lerp(transform.position, Prev.transform.position + Prev.transform.TransformVector(offset), Time.deltaTime * 10);
+       // transform.rotation = Quaternion.LookRotation(dir,transform.up);
+        float rDotu = Vector3.Dot(transform.right, Prev.transform.up);
+        //transform.Rotate(0, 0, -rDotu*Time.deltaTime*200);
+    }
+    public void Spawn(FollowChainFinal from) {
+        if(Prev!= null) {
+            joint = new GameObject();
+            joint.transform.parent = transform.parent;
+            joint.transform.localPosition = transform.localPosition;
+            joint.transform.rotation = transform.rotation;
+            offset = transform.localPosition;
+            transform.parent = null;
+        }
+        //GameObject go = GameObject.CreatePrimitive(PrimitiveType.Cube);
+        if (transform.childCount == 0) return;
+        Transform trans = transform.GetChild(0);
+        if (trans == null) return;
+        FollowChainFinal fc = trans.gameObject.AddComponent<FollowChainFinal>();
+        
+        Next = fc;
+        fc.Prev = this;
+        fc.Spawn(this);
+
+    }
+}
