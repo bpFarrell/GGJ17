fd4a84d9f12161ae2b6c80fcdf0eefd8b3e05f70
Brandon Farrell <brandonf@bentimagelab.com>
2017-01-21 11:22:30 -0800

    Proper chain rotation.

--- Assets/_GGJ/Scripts/Player/FollowChainFinal.cs
+++ Assets/_GGJ/Scripts/Player/FollowChainFinal.cs
@@ -7,12 +7,17 @@ public class FollowChainFinal : MonoBehaviour {
     public float distance {
         get { return offset.magnitude; }
     }
-
+    public static GameObject rootNode;
     public FollowChainFinal Next;
     public FollowChainFinal Prev;
     public GameObject joint;
+    public GameObject dummy;
+    public float stiffness = 1;
     public Vector3 offset;
     void Awake() {
+        if (rootNode == null) {
+            rootNode = new GameObject("RootNode");
+        }
         if (length == -1) return;
         Spawn(null);
     }
@@ -24,20 +29,20 @@ public class FollowChainFinal : MonoBehaviour {
         if (dist < distance) {
             transform.position = Prev.transform.position + dir * distance;
         }
-        transform.position = Vector3.Lerp(transform.position, Prev.transform.position + Prev.transform.TransformVector(offset), Time.deltaTime * 10);
-        transform.rotation = Quaternion.LookRotation(dir,transform.up);
-        transform.Rotate(0, -90, 0);
-        float rDotu = Vector3.Dot(transform.forward, Prev.transform.up);
-        transform.Rotate(0, 0, rDotu*Time.deltaTime*200);
+        dummy.transform.position = Vector3.Lerp(dummy.transform.position, Prev.transform.position + Prev.transform.TransformVector(offset), Time.deltaTime * 10 * stiffness);
+        dummy.transform.rotation = Quaternion.LookRotation(dir,dummy.transform.up);
+        float rDotu = Vector3.Dot(dummy.transform.right, Prev.transform.up);
+        dummy.transform.Rotate(0, 0, -rDotu*Time.deltaTime*200*stiffness);
     }
     public void Spawn(FollowChainFinal from) {
         if(Prev!= null) {
-            joint = new GameObject();
-            joint.transform.parent = transform.parent;
-            joint.transform.localPosition = transform.localPosition;
-            joint.transform.rotation = transform.rotation;
+            dummy = new GameObject(transform.name);
+            dummy.transform.parent = rootNode.transform;
+            dummy.transform.position = transform.position;
+            dummy.transform.rotation= Quaternion.LookRotation(transform.position-Prev.transform.position, Vector3.up);
             offset = transform.localPosition;
-            transform.parent = null;
+            stiffness = Prev.stiffness;
+            transform.parent = dummy.transform;
         }
         //GameObject go = GameObject.CreatePrimitive(PrimitiveType.Cube);
         if (transform.childCount == 0) return;
