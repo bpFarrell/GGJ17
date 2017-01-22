2cb2e322c6574b69bc7bc03f54da776fadadf933
Rojibora <Rojibora@gmail.com>
2017-01-22 15:13:33 -0800

    Needed Arena Scene changes for dual screen.

---
+++ Assets/lookAt.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class lookAt : MonoBehaviour {
+
+	public Transform focus;
+
+	// Use this for initialization
+	void Start () {
+		transform.LookAt(focus.position);
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		transform.LookAt(focus.position);
+	}
+}
