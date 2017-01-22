460618954bd2bd10908fa374aed141458c07acac
Rojibora <Rojibora@gmail.com>
2017-01-21 20:17:48 -0800

    Camera behaviour added

---
+++ Assets/cameraMovement.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cameraMovement : MonoBehaviour 
+{
+	public float followSpeed;
+	public Transform cameraFollow;
+	public Transform lookTarget;
+
+	// Use this for initialization
+	void Start () 
+	{
+		transform.position = cameraFollow.position;
+		transform.LookAt(lookTarget.position);
+	}
+
+	// Update is called once per frame
+	void Update () 
+	{
+		Vector3 displacement = cameraFollow.position - transform.position;
+		float step = displacement.magnitude * followSpeed/100;
+
+		Vector3 tempPos = transform.position;
+		tempPos += (displacement.normalized * step);
+
+		transform.position = tempPos;
+		transform.LookAt(lookTarget.position);
+	}
+}
