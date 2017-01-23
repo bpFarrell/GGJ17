c9edddb27ffae0875ec37fb9f2f5cc2c56e9e3bf
Vachara Nick Lertackakorn <vakara@hotmail.com>
2017-01-22 17:30:06 -0800

    ink attack

---
+++ Assets/InkAttack.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InkAttack : MonoBehaviour {
+
+    void OnTriggerEnter(Collider col) {
+        NodeMain nm;
+        if ((nm = col.GetComponent<NodeMain>()) != null) {
+            Debug.Log("drop it");
+            nm.SetToNeutralState();
+            
+        }
+    }
+}
---
+++ Assets/KillMeBro.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KillMeBro : MonoBehaviour {
+
+    public float timeToDie;
+	// Use this for initialization
+	void Start () {
+        Destroy(gameObject, timeToDie);
+	}
+}
