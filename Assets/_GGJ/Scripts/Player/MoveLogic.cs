5eaa730ea5b34ae761e4071d05d1ae18a7d169a5
Brandon Farrell <Brandon@BPFarrell.com>
2017-02-05 19:19:10 -0800

    Delayed squiding.

--- Assets/_GGJ/Scripts/Player/MoveLogic.cs
+++ Assets/_GGJ/Scripts/Player/MoveLogic.cs
@@ -4,13 +4,103 @@ using UnityEngine;
 
 public class MoveLogic : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
+    public Vector3 velocity = new Vector3();
+    public float friction = 1;
+    public float power = 20;
+    float lastPressTime;
+    GameObject roll;
+    float twistDelay = 0.2f;
+    float twist = 360;
+    float psTime;
+    bool queued = false;
+    float timeToPush;
+    // Use this for initialization
+    void Awake () {
+
+        roll = transform.GetChild(0).gameObject;
+    }
 	
 	// Update is called once per frame
 	void Update () {
-		
-	}
+
+        CalculateIdle();
+        BoundCheck();
+        if (Time.time > timeToPush&&queued)
+            ApplyForce();
+        float deltaTwist = CalculateTwist();
+        float levelOut = LevelOut();
+        roll.transform.Rotate(0, 0, deltaTwist);
+
+
+        transform.position += velocity * Time.deltaTime;
+        velocity = velocity - (velocity * Time.deltaTime * friction);
+    }
+    public void Go() {
+        PrepGo();
+        if (lastPressTime + twistDelay > Time.time) {
+            if (twist > 0)
+                twist -= 360;
+        }
+        lastPressTime = Time.time;
+    }
+    void PrepGo() {
+        if (queued) return;
+        queued = true;
+        timeToPush = Time.time + 0.2f;
+    }
+    void ApplyForce() {
+        queued = false;
+        velocity += transform.forward * power;
+    }
+    float CalculateTwist() {
+
+        float old = twist;
+        twist += Time.deltaTime * 500;
+        twist = Mathf.Min(twist, 360);
+        return old - twist;
+    }
+    float LevelOut() {
+        return Vector3.Dot(transform.forward, Vector3.up);
+    }
+    void CalculateIdle() {
+        float scale = 1;
+        float speed = 1;
+        Vector3 offset = new Vector3(
+            Mathf.Sin(Time.time * speed) * scale,
+            Mathf.Cos(Time.time * speed * 1.3f) * scale,
+            -Mathf.Cos(Time.time * speed) * scale
+            );
+        roll.transform.localPosition = offset;
+    }
+    void BoundCheck() {
+        float force = 100;
+        if (transform.position.x < 250) {
+            ForceParticle(90);
+            velocity.x += Time.deltaTime * force;
+        }
+        if (transform.position.z < 250) {
+            ForceParticle(0);
+            velocity.z += Time.deltaTime * force;
+        }
+        if (transform.position.x > 750) {
+            ForceParticle(90);
+            velocity.x -= Time.deltaTime * force;
+        }
+        if (transform.position.z > 750) {
+            ForceParticle(0);
+            velocity.z -= Time.deltaTime * force;
+        }
+    }
+    void ForceParticle(float yRotate) {
+        if (Time.time > psTime + 1.0f) {
+            psTime = Time.time;
+            GameObject ps = Instantiate(Resources.Load("psStop")) as GameObject;
+            ps.transform.rotation = Quaternion.AngleAxis(yRotate, Vector3.up);// (Vector3.up, yRotate);
+                                                                              //     ParticleSystem.MainModule mainMod = ps.GetComponent<ParticleSystem>().main;
+                                                                              //     mainMod.startRotation3D = true;
+                                                                              //     mainMod.startRotationY = yRotate;
+            ps.transform.position = transform.position;
+
+        }
+    }
 }
--- Assets/_GGJ/Scripts/Player/PlayerController.cs
+++ Assets/_GGJ/Scripts/Player/PlayerController.cs
@@ -8,26 +8,18 @@ public class PlayerController : MonoBehaviour {
         CamerarRlative
     }
     public InputType inputType = InputType.CamerarRlative;
-    float psTime;
     public bool isPlayerOne;
 	public string playerPrefix = "P1_";
-    public Vector3 velocity = new Vector3();
-    public float friction = 1;
-    public float power = 5;
-    float lastPressTime;
-    GameObject roll;
-    float twistDelay = 0.2f;
     public IList<GameObject> tents = new List<GameObject>();
     public delegate void func();
-    float twist = 360;
     public event func flutter;
     public static bool locked;
     public TorsoLogic torso;
     public Camera cam;
+    MoveLogic ml;
     void Awake() {
         Mesh mesh;
         locked = false;
-        roll = transform.GetChild(0).gameObject;
         Animation[] anim = GetComponentsInChildren<Animation>();
         for(int x = 0; x < anim.Length; x++) {
             anim[x].Play();
@@ -37,6 +29,7 @@ public class PlayerController : MonoBehaviour {
         if (torso == null) {
             Debug.LogError("Torse not set! Attack torso logic to octoTorso");
         }
+        ml = gameObject.AddComponent<MoveLogic>();
     }
 	void Start () {
         Transform[] children = GetComponentsInChildren<Transform>();
@@ -53,33 +46,19 @@ public class PlayerController : MonoBehaviour {
         float angle = 0;
         if (!locked) {
             if (Input.GetButtonDown(playerPrefix + "A") || Input.GetKeyDown(KeyCode.Space)) {
-                velocity += transform.forward * power;
+                ml.Go();
                 flutter();
                 torso.Woosh();
-                if (lastPressTime + twistDelay > Time.time) {
-                    if (twist > 0)
-                        twist -= 360;
-                }
-                lastPressTime = Time.time;
             }
             angle = Input.GetAxis(playerPrefix + "LeftHorizontal");
             if (Input.GetKey(KeyCode.A)) angle = -1;
             if (Input.GetKey(KeyCode.D)) angle = 1;
         }
-        CalculateIdle();
-        BoundCheck();
-        float deltaTwist = CalculateTwist();
-        float levelOut = LevelOut();
         if (inputType == InputType.CamerarRlative) {
             transform.rotation = Quaternion.RotateTowards(transform.rotation, CalculateInput(), Time.deltaTime * 125);
         } else if (inputType == InputType.CamerarRlative) {
             transform.Rotate(0, angle * Time.deltaTime*100, 0);
         }
-        roll.transform.Rotate(0, 0, deltaTwist);
-        
-
-        transform.position += velocity * Time.deltaTime;
-        velocity = velocity - (velocity * Time.deltaTime*friction);
 	}
     Quaternion CalculateInput() {
         Vector3 camForward = cam.transform.forward;
@@ -110,57 +89,5 @@ public class PlayerController : MonoBehaviour {
             target = transform.forward;
         }
         return Quaternion.LookRotation(target,transform.up);
-    }
-    float CalculateTwist() {
-
-        float old = twist;
-        twist += Time.deltaTime * 500;
-        twist = Mathf.Min(twist, 360);
-        return  old - twist;
-    }
-    float LevelOut() {
-        return Vector3.Dot(transform.forward, Vector3.up);
-    }
-    void CalculateIdle() {
-        float scale = 1;
-        float speed = 1;
-        Vector3 offset = new Vector3(
-            Mathf.Sin(Time.time * speed) * scale,
-            Mathf.Cos(Time.time * speed*1.3f) * scale,
-            - Mathf.Cos(Time.time * speed) * scale
-            );
-        roll.transform.localPosition = offset;
-    }
-    void BoundCheck() {
-        float force=100;
-        if (transform.position.x < 250) {
-            ForceParticle(90);
-            velocity.x += Time.deltaTime * force;
-        }
-        if (transform.position.z < 250) {
-            ForceParticle(0);
-            velocity.z += Time.deltaTime * force;
-        }
-        if (transform.position.x > 750) {
-            ForceParticle(90);
-            velocity.x -= Time.deltaTime * force;
-        }
-        if (transform.position.z > 750) {
-            ForceParticle(0);
-            velocity.z -= Time.deltaTime * force;
-        }
-    }
-    void ForceParticle(float yRotate) {
-        if (Time.time > psTime + 1.0f)
-        {
-            psTime = Time.time;
-            GameObject ps = Instantiate(Resources.Load("psStop")) as GameObject;
-            ps.transform.rotation = Quaternion.AngleAxis(yRotate, Vector3.up);// (Vector3.up, yRotate);
-       //     ParticleSystem.MainModule mainMod = ps.GetComponent<ParticleSystem>().main;
-       //     mainMod.startRotation3D = true;
-       //     mainMod.startRotationY = yRotate;
-            ps.transform.position = transform.position;
-
-        }
     }
 }
