fc3a15e9429f634d593d81c0a104c2708ecf153f
Brandon Farrell <Brandon@BPFarrell.com>
2017-02-08 22:22:51 -0800

    Loading Bar

---
+++ Assets/_GGJ/Scripts/Utility/LoadingBar.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LoadingBar : MonoBehaviour {
+    public float t = 0;
+    Material mat;
+    private void Awake() {
+        mat = GetComponent<MeshRenderer>().material;
+    }
+
+    // Update is called once per frame
+    void Update () {
+        mat.SetVector("_T", new Vector4(t, 0, 0, 0));
+	}
+}
