e7eea3b912a472d618264ee83e627fc72a5f67c7
Rojibora <Rojibora@gmail.com>
2017-02-10 16:11:25 -0800

    Disabled player cameras when the game is finished

---
+++ Assets/DisableOnEnd.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DisableOnEnd : MonoBehaviour {
+
+	// Use this for initialization
+	void Awake () 
+	{
+		GameStateMachine._Main.sEnd.StartOfState += () => gameObject.SetActive(false);
+	}
+}
